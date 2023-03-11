469ef188132f1b593d956805211c26de822a1733
Mohammad Ibrahim <112249309+awesomecoderdev@users.noreply.github.com>
2023-03-12 02:04:21 +0600

    Updated Manifest

--- assets/js/background.js
+++ assets/js/background.js
@@ -1,9 +1,1 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!******************************!*\
-  !*** ./src/js/background.js ***!
-  \******************************/
 console.log("Background script loaded");
-/******/ })()
-;
-//# sourceMappingURL=background.js.map
